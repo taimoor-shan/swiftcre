e9cbef05650a2b5e4e1ac79f7b748bdf666bb81a
taimoor-shan <tshanajk@gmail.com>
2024-06-05 06:49:01 +0500

    Analytics added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import { RootLayout } from '@/components/RootLayout'
-
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/tailwind.css'
 
 export const metadata = {
@@ -13,7 +13,9 @@ export default function Layout({ children }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
-        <RootLayout>{children}</RootLayout>
+        <RootLayout>{children}
+        <Analytics />
+        </RootLayout>
       </body>
     </html>
   )
